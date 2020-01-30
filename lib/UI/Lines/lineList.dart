32233881ffc4f933bdccfc54dd1fef9c1d4c1ac8
Bahaaib <bahaasco@gmail.com>
2020-01-30 15:48:50 +0200

    added network messages UI

--- lib/UI/Lines/lineList.dart
+++ lib/UI/Lines/lineList.dart
@@ -5,6 +5,7 @@ import 'package:ocpua_app/UI/Lines/lineItem.dart';
 import 'package:ocpua_app/bloc/line/bloc.dart';
 import 'package:ocpua_app/bloc/signals/bloc.dart';
 import 'package:ocpua_app/resources/string.dart';
+import 'package:progress_dialog/progress_dialog.dart';
 
 class LineList extends StatefulWidget {
   @override
@@ -15,13 +16,27 @@ class _LineListState extends State<LineList> {
   List<Line> linesList = List<Line>();
   final LineBloc _linesBloc = GetIt.instance<LineBloc>();
   final SignalsBloc _signalsBloc = GetIt.instance<SignalsBloc>();
+  bool _isLoading = false;
+  bool _isTimedOut = false;
+  bool _isNotResponding = true;
+  String _timeoutMessage = '';
+  ProgressDialog _progressDialog;
 
   void initState() {
+    _isLoading = true;
+    WidgetsBinding.instance.addPostFrameCallback((_) => _progressDialog.show());
+
     _linesBloc.linesStateSubject.listen((receivedState) {
       if (receivedState is LinesAreFetched) {
         setState(() {
+          _isLoading = false;
+          _isNotResponding = false;
           linesList = receivedState.linesList;
         });
+
+        if (_progressDialog.isShowing() && !_isLoading) {
+          _progressDialog.dismiss();
+        }
       }
     });
     _linesBloc.dispatch(LinesRequested());
@@ -29,8 +44,62 @@ class _LineListState extends State<LineList> {
     super.initState();
   }
 
+  void _initProgressDialog() {
+    _progressDialog.style(
+      message: 'Collecting Client Data...',
+      backgroundColor: Colors.white,
+      progressWidget: CircularProgressIndicator(),
+      elevation: 10.0,
+      insetAnimCurve: Curves.easeInOut,
+      messageTextStyle: TextStyle(
+          color: Colors.black, fontSize: 14.0, fontWeight: FontWeight.w600),
+    );
+  }
+
+  Future<void> _launchTimeoutCounter(BuildContext context) async {
+    Future.delayed(Duration(seconds: 10)).then((_) {
+      _progressDialog.dismiss();
+      setState(() {
+        _timeoutMessage =
+            'No Lines Listed for your account yet. Please contact system admin for further assistance';
+      });
+      if (_isNotResponding) {
+        showDialog(
+          context: context,
+          builder: (_) => AlertDialog(
+            content: Container(
+              height: 100.0,
+              child: Center(
+                child: Text('Connection Timeout'),
+              ),
+            ),
+            title: Container(
+              height: 50.0,
+              color: Colors.red,
+              child: Center(
+                child: Text(
+                  'Error',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            titlePadding: EdgeInsets.all(0.0),
+          ),
+        );
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    _progressDialog = ProgressDialog(context,
+        type: ProgressDialogType.Normal, isDismissible: false, showLogs: false);
+    _initProgressDialog();
+    if (!_isTimedOut) {
+      _launchTimeoutCounter(context);
+      _isTimedOut = true;
+    }
+
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
@@ -38,19 +107,36 @@ class _LineListState extends State<LineList> {
         title: Text('Welcome'),
         centerTitle: true,
       ),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            child: ListView.builder(
-                padding: EdgeInsets.only(top: 20),
-                itemCount: linesList.length,
-                itemBuilder: (BuildContext context, int i) {
-                  return LineItem(line: linesList[i]);
-                }),
-          ),
-          logoutButton(),
-        ],
-      ),
+      body: linesList.isNotEmpty
+          ? Column(
+              children: <Widget>[
+                Expanded(
+                  child: ListView.builder(
+                      padding: EdgeInsets.only(top: 20),
+                      itemCount: linesList.length,
+                      itemBuilder: (BuildContext context, int i) {
+                        return LineItem(line: linesList[i]);
+                      }),
+                ),
+                logoutButton(),
+              ],
+            )
+          : Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(
+                  Icons.warning,
+                  size: 50.0,
+                  color:
+                      _timeoutMessage.isNotEmpty ? Colors.grey : Colors.white,
+                ),
+                Text(
+                  _timeoutMessage,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ],
+            ),
     );
   }
 
@@ -72,6 +158,4 @@ class _LineListState extends State<LineList> {
       ),
     );
   }
-
-
 }
--- lib/UI/Lines/signalList.dart
+++ lib/UI/Lines/signalList.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
-import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'package:progress_dialog/progress_dialog.dart';
 import 'package:ocpua_app/PODO/Line.dart';
 import 'package:ocpua_app/PODO/Signal.dart';
 import 'package:ocpua_app/UI/Lines/SignalItem.dart';
@@ -21,9 +21,15 @@ class _SignalListState extends State<SignalList> {
   final _signalsBloc = GetIt.instance<SignalsBloc>();
   final _signalsList = List<Signal>();
   bool _isLoading = false;
+  bool _isTerminationRequested = false;
+  bool _isTimedOut = false;
+  bool _isNotResponding = true;
+  String _timeoutMessage = '';
+  ProgressDialog _progressDialog;
 
   @override
   void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) => _progressDialog.show());
     print('SignalsList --> initState()');
     _isLoading = true;
     _signalsBloc.signalsStateSubject.listen((receivedState) {
@@ -31,10 +37,17 @@ class _SignalListState extends State<SignalList> {
         if (mounted) {
           setState(() {
             _isLoading = false;
+            _isNotResponding = false;
             _signalsList.clear();
             _signalsList.addAll(receivedState.signalsList);
             print('SIGNALS SIZE = ${_signalsList.length}');
           });
+
+          if (_progressDialog.isShowing() &&
+              !_isLoading &&
+              !_isTerminationRequested) {
+            _progressDialog.dismiss();
+          }
         }
       }
     });
@@ -54,40 +67,121 @@ class _SignalListState extends State<SignalList> {
     super.initState();
   }
 
+  void _initProgressDialog({@required String message}) {
+    _progressDialog.style(
+      message: message,
+      backgroundColor: Colors.white,
+      progressWidget: CircularProgressIndicator(),
+      elevation: 10.0,
+      insetAnimCurve: Curves.easeInOut,
+      messageTextStyle: TextStyle(
+          color: Colors.black, fontSize: 14.0, fontWeight: FontWeight.w600),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    _progressDialog = ProgressDialog(context,
+        type: ProgressDialogType.Normal, isDismissible: false, showLogs: false);
+    _initProgressDialog(message: 'Connecting OPCUA Server...');
+    if (!_isTimedOut) {
+      _launchTimeoutCounter(context);
+      _isTimedOut = true;
+    }
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.red,
         title: Text('${widget.line.name}'),
         centerTitle: true,
-      ),
-      body: ModalProgressHUD(
-        inAsyncCall: _isLoading,
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              child: ListView.builder(
-                  padding: EdgeInsets.only(top: 20),
-                  itemCount:
-                      _signalsList.length != null ? _signalsList.length : 0,
-                  itemBuilder: (BuildContext context, int position) {
-                    return SignalItem(
-                      signal: _signalsList[position],
-                      index: position,
-                    );
-                  }),
-            )
-          ],
+        leading: IconButton(
+          icon: Icon(
+            Icons.close,
+            color: Colors.white,
+          ),
+          onPressed: () async => await _terminateSession(context),
         ),
       ),
+      body: _signalsList.isNotEmpty
+          ? Column(
+              children: <Widget>[
+                Expanded(
+                  child: ListView.builder(
+                      padding: EdgeInsets.only(top: 20),
+                      itemCount:
+                          _signalsList.length != null ? _signalsList.length : 0,
+                      itemBuilder: (BuildContext context, int position) {
+                        return SignalItem(
+                          signal: _signalsList[position],
+                          index: position,
+                        );
+                      }),
+                )
+              ],
+            )
+          : Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(
+                  Icons.warning,
+                  size: 50.0,
+                  color:
+                      _timeoutMessage.isNotEmpty ? Colors.grey : Colors.white,
+                ),
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  child: Text(
+                    _timeoutMessage,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                ),
+              ],
+            ),
     );
   }
 
-  @override
-  void dispose() {
-    print('DISPOSED');
+  Future<void> _launchTimeoutCounter(BuildContext context) async {
+    Future.delayed(Duration(seconds: 10)).then((_) {
+      _progressDialog.dismiss();
+      setState(() {
+        _timeoutMessage =
+            'No Signals Listed for this line yet. Please contact system admin for further assistance';
+      });
+      if (_isNotResponding) {
+        showDialog(
+          context: context,
+          builder: (_) => AlertDialog(
+            content: Container(
+              height: 100.0,
+              child: Center(
+                child: Text('Connection Timeout'),
+              ),
+            ),
+            title: Container(
+              height: 50.0,
+              color: Colors.red,
+              child: Center(
+                child: Text(
+                  'Error',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            titlePadding: EdgeInsets.all(0.0),
+          ),
+        );
+      }
+    });
+  }
+
+  Future<void> _terminateSession(BuildContext context) async {
+    _initProgressDialog(message: 'Closing Session...');
+    _progressDialog.show();
+    _isTerminationRequested = true;
     _signalsBloc.dispatch(SessionTerminationRequested());
-    super.dispose();
+    Future.delayed(Duration(seconds: 4)).then((_) {
+      _progressDialog.dismiss();
+      Navigator.of(context).pop();
+    });
   }
 }
